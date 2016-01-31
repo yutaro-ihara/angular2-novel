24e5f8c8ef79d800ddb07129bfa8f10441396732
yutaro-ihara <yutaro.ihara@medpeer.co.jp>
2016-01-31 17:34:00 +0900

    delete unused files

--- app/config.ts
+++
@@ -1,4 +0,0 @@
-export interface Config {
-  title: string;
-  titleBg: string;
-}
--- app/mock-config.ts
+++
@@ -1,6 +0,0 @@
-import {Config} from './config';
-
-export var CONFIG: Config = {
-  title: 'ほげほげ',
-  titleBg: 'gazounaiyo.naiyo'
-};
